f9432b1525d28e482e76233c1ab634233fb2d656
Pedro Muniz <pedromunizcherulli3@gmail.com>
2026-06-12 20:36:43 -0300

    start.sh fixed

--- rme/start.sh
+++ rme/start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
+set -e
 
 echo "Iniciando API..."
-python scripts/python/api_ner.py &
-PID_API=$!
+python /app/scripts/python/api_ner.py &
+PID_API=\$!
 
-echo "Renderizando o relatório inicial..."
-cd /app
-quarto render scripts/qmd/relatorio_rme.qmd --output-dir ../../outputs/html
+echo "Renderizando o relatorio inicial..."
+quarto render /app/scripts/qmd/relatorio_rme.qmd --output-dir /app/outputs/html
 
 echo "Iniciando servidor HTTP na porta 4200..."
-cd outputs/html/
+cd /app/outputs/html
 python -m http.server 4200 &
-PID_HTTP=$!
+PID_HTTP=\$!
 
-trap "echo 'Encerrando...'; kill $PID_API $PID_HTTP 2>/dev/null; exit 0" SIGINT SIGTERM
+trap "echo 'Encerrando...'; kill \ \ 2>/dev/null; exit 0" SIGINT SIGTERM
 
 echo "Sistema Iniciado!"
 echo "Rotulador: http://localhost:5000"
-echo "Relatório: http://localhost:4200"
+echo "Relatorio: http://localhost:4200"
 
 wait
